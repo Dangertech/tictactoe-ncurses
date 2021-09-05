8780be23efe73d01cb2661b5f9045407089bf04f
Dangertech <dangertech@e.email>
2021-09-05 22:33:07 +0200

    Removed grid size limit and added (some) warnings instead

--- rendering.cpp
+++ rendering.cpp
@@ -9,12 +9,16 @@ void start_menu()
 {
 	// Create a window that the menu will be displayed on
 	WINDOW *menu;
-	// Names for the options on the left side of the window
-	std::vector <std::string> display_options { "Rows", "Columns", "Pixels needed to win" };
-	// Values to display on the right side of the window
-	std::vector <int> display_values { grid_size_y, grid_size_x, pixels_needed };
+	struct menu_options
+	{
+		std::string description;
+		int display_value;
+	};
+	 
+	// Construct a vector with the options
+	std::vector <menu_options> start_menu = { {"Rows", grid_size_y}, { "Columns", grid_size_x}, {"Piexls needed to win", pixels_needed} };
 	
-	int window_rows = display_options.size() + 4, window_columns = 50;
+	int window_rows = start_menu.size() + 4, window_columns = 50;
 	int y_borders = 2; // Space to be left out before the first entry gets rendered
 	int x_origin = (max_x / 2) - ( window_columns / 2), y_origin = 10;
 	int selected_entry = 0;
@@ -29,30 +33,73 @@ void start_menu()
 					selected_entry--;
 				break;
 			case KEY_DOWN:
-				if (selected_entry < display_options.size()-1)
+				if (selected_entry < start_menu.size()-1)
 					selected_entry++;
 				break;
 			case KEY_LEFT:
-				if (display_values[selected_entry] > 2)
-					display_values[selected_entry]--;
+				start_menu[selected_entry].display_value--;
 				break;
 			case KEY_RIGHT:
-				if (display_values[selected_entry] < 30)
-					display_values[selected_entry]++;
+				start_menu[selected_entry].display_value++;
 				break;
 		}
-		// Check if selected_entry got out of bounds
+		 
 		// Create a title and some decorations outside of the window
 		mvprintw( max_y - 5, 2, "%d", selected_entry);
 		mvcprintw( 3, "TIC TAC TOE");
 		mvprintw( y_origin-1, x_origin, "Starting a new game:");
 		mvprintw( y_origin + window_rows, x_origin, "Press 'Enter' to confirm your choices!");
+		 
+		 
+		// Make a vector that holds possible error message
+		std::vector <std::string> error_msgs;
+		error_msgs.clear();
+		// Push errors back into the error vector
+		for (int i = 0; i < 2; i++)
+		{
+			int val = start_menu[i].display_value;
+			// I know, this is pretty bad code but it's just a little easter egg
+			if (val < 2)
+			{
+				error_msgs.push_back("Sizes smaller than two are not supported!");
+				error_msgs.push_back("You may experience crashes and unexpected behaviour!");
+			}
+			else if (val > 30 && val < 120)
+			{
+				error_msgs.push_back("You need a sufficiently large screen to properly display high grid sizes!");
+				error_msgs.push_back("You may experience unexpected behaviour!");
+			}
+			else if (val > 120 && val < 235)
+				error_msgs.push_back("I don't think there's a screen that large...");
+			else if (val > 235 && val < 512)
+				error_msgs.push_back("Ok, you're bored, right?");
+			else if (val > 512 && val < 828)
+				error_msgs.push_back("Are you trying to impress me through your waste of time?");
+			else if (val > 828 && val < 1021)
+				error_msgs.push_back("This is getting ridiculous.");
+			else if (val > 1021 && val < 1372)
+				error_msgs.push_back("You might run into some performance issues now, btw");
+			else if (val > 1372 && val < 1845)
+				error_msgs.push_back("I really don't have time for this.");
+			else if (val > 1845)
+				error_msgs.push_back("Don't you have hobbies or something!?");
+		} 
+		// Print error messages
+		erase();
+		attron( COLOR_PAIR(2) );
+		for (int i = 0; i < error_msgs.size(); i++)
+		{
+			mvprintw( y_origin + window_rows + 2 + i, x_origin + 3, error_msgs[i].c_str() );
+		}
+		attroff( COLOR_PAIR(2) );
+		 
+		 
 		// Erase the entire window in case there was a menu entry with a higher digit count before than the one now
 		// For example: A value was 10, now the user presses KEY_LEFT, but the menu shows 90 although it is nine, because the location of the '0' wasn't redrawn
 		werase( menu );
 		// Print a box around the menu
 		box(menu, 1, 0);
-		for (int i = 0; i < display_options.size(); i++)
+		for (int i = 0; i < start_menu.size(); i++)
 		{
 			if (selected_entry == i)
 			{
@@ -60,8 +107,8 @@ void start_menu()
 				mvwprintw( menu, i + y_borders, getmaxx(menu)-6, ">");
 				wattron( menu, A_STANDOUT );
 			}
-			mvwprintw( menu, i + y_borders, 2, display_options[i].c_str() );
-			mvwprintw( menu, i + y_borders, getmaxx(menu) - 8, "%d", display_values[i] );
+			mvwprintw( menu, i + y_borders, 2, start_menu[i].description.c_str() );
+			mvwprintw( menu, i + y_borders, getmaxx(menu) - 8, "%d", start_menu[i].display_value );
 			wattroff( menu, A_STANDOUT );
 		}
 		refresh();
@@ -70,9 +117,9 @@ void start_menu()
 	}
 	// OK, this looks very ugly, but I couldn't find a better solution:
 	// Assign every real variable the variable in the vector
-	grid_size_y = display_values[0];
-	grid_size_x = display_values[1];
-	pixels_needed = display_values[2];
+	grid_size_y = start_menu[0].display_value;
+	grid_size_x = start_menu[1].display_value;
+	pixels_needed = start_menu[2].display_value;
 	// Delete the window and clear the screen
 	delwin( menu );
 	erase();
